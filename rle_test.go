535fd07207e46f5d5d083614b3b48bccc110a813
TJ Holowaychuk <tj@vision-media.ca>
2016-11-14 13:09:56 -0800

    add nested benchmarks

--- rle_test.go
+++ rle_test.go
@@ -35,28 +35,59 @@ func TestInt64(t *testing.T) {
 }
 
 func TestInt64Cardinality(t *testing.T) {
-	{
-		nums := []int64{1, 1, 1, 1, 1, 1, 0, 0, 0, 2}
-		b := rle.Int64(nums)
-		v, err := rle.Int64Card(b)
-		assert.NoError(t, err)
-		assert.Equal(t, map[int64]uint64{1: 6, 0: 3, 2: 1}, v)
-	}
+	nums := []int64{1, 1, 1, 1, 1, 1, 0, 0, 0, 2}
+	b := rle.Int64(nums)
+	v, err := rle.Int64Card(b)
+	assert.NoError(t, err)
+	assert.Equal(t, map[int64]uint64{1: 6, 0: 3, 2: 1}, v)
 }
 
 func BenchmarkInt64(b *testing.B) {
-	nums := []int64{1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2}
-	for i := 0; i < b.N; i++ {
-		rle.Int64(nums)
-	}
+	nums100 := make([]int64, 100e3)
+	nums500 := make([]int64, 500e3)
+	nums1000 := make([]int64, 1e6)
+
+	b.Run("100k", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			rle.Int64(nums100)
+		}
+	})
+
+	b.Run("500k", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			rle.Int64(nums500)
+		}
+	})
+
+	b.Run("1M", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			rle.Int64(nums1000)
+		}
+	})
 }
 
 func BenchmarkInt64Values(b *testing.B) {
-	nums := []int64{1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2}
-	buf := rle.Int64(nums)
-	for i := 0; i < b.N; i++ {
-		rle.Int64Values(buf)
-	}
+	nums100 := rle.Int64(make([]int64, 100e3))
+	nums500 := rle.Int64(make([]int64, 500e3))
+	nums1000 := rle.Int64(make([]int64, 1e6))
+
+	b.Run("100k", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			rle.Int64Values(nums100)
+		}
+	})
+
+	b.Run("500k", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			rle.Int64Values(nums500)
+		}
+	})
+
+	b.Run("1M", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			rle.Int64Values(nums1000)
+		}
+	})
 }
 
 func BenchmarkInt64Card(b *testing.B) {
